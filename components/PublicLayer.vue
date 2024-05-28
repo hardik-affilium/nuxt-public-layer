accea96e447648e2ad5bdf72b3591bc8a57a31ba
hardik <hrdkzala@gmail.com>
2024-05-28 12:07:19 +0530

    public layer changes

--- components/PublicLayer.vue
+++ components/PublicLayer.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <h1>Hello</h1>
-    <pre>
-This is called from Nuxt Public Layer: https://github.com/hardik-affilium/nuxt-public-layer</pre
-    >
+    <p>
+      This is called from Nuxt Public Layer:
+      https://github.com/hardik-affilium/nuxt-public-layer
+    </p>
+    <v-container>
+      <v-row>
+        <v-col cols="12" class="text-center"> This is testing row. </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
